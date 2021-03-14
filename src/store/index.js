78ab2747fe131f9a2251173e572628a2fe0549c4
Lukenoutte <lukenoutte@gmail.com>
2021-03-14 18:00:52 -0300

    vuex mutations

--- src/store/index.js
+++ src/store/index.js
@@ -6,16 +6,20 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         itensArray: [
-            { image: "/", name: "Shoes", price: "10" },
-            { image: "/", name: "Table", price: "30" },
-            { image: "/", name: "Coach", price: "30" },
-            { image: "/", name: "Notebook", price: "100" },
-            { image: "/", name: "TV", price: "100" },
+            { id: 0, image: "/", name: "Shoes", price: "10" },
+            { id: 1, image: "/", name: "Table", price: "30" },
+            { id: 2, image: "/", name: "Coach", price: "30" },
+            { id: 3, image: "/", name: "Notebook", price: "100" },
+            { id: 4, image: "/", name: "TV", price: "100" },
         ],
         cartItens: []
     },
     mutations: {
-
+        addItemToCart: (state, id) => {
+            let itemToAdd = state.itensArray.find(itemInArray => itemInArray.id === id);
+            state.cartItens.push(itemToAdd);
+            console.log(state.cartItens);
+        }
     }
 })
 
